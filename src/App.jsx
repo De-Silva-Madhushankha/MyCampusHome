68bee7abe0e61c8a1290b94cae623dfc8c05b1ea
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-12-20 11:15:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        <p> Hello!, Welcome to RentNearUni, A convinient platform to find an accomodation near your uni without going door to door</p>
+      </h1>
+        
+    </>
+  )
+}
+
+export default App
