5b41a9eac3d926ddfdd93af76aa079ab0a6a7174
Chamoda Pramudika <pramudika0305@gmail.com>
2025-02-03 12:10:38 +0530

    Update Hero component styles and enhance NegotiateRent,ApplyOnline,ScheduleTour pages

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
     return (
         <section className="relative overflow-hidden">
         <div
-          className="absolute inset-0 bg-gradient-to-r from-purple-600 to-purple-400 opacity-90"
+          className="absolute inset-0 bg-gradient-to-b from-white to-indigo-50 opacity-90"
           style={{
             backgroundImage: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-Pnvtek5Km2IbmgQjHKX8DEedeEzVgD.png",
             backgroundSize: "cover",
@@ -10,13 +10,13 @@ const Hero = () => {
           }}
         />
         <div className="relative container text-center py-24 md:py-32">
-          <div className="max-w-3xl mx-auto text-center text-white">
-            <h1 className="text-4xl text-center md:text-5xl lg:text-6xl  font-bold mb-6">
+          <div className="max-w-3xl mx-auto text-center text-gray-900">
+            <h1 className="text-4xl text-center md:text-5xl lg:text-5xl  font-bold mb-6">
               New Approach to
               <br />
               Application Process
             </h1>
-            <p className="text-lg text-center md:text-xl opacity-90">
+            <p className="text-lg text-center md:text-xl text-gray-600 opacity-90">
               Complete your rental application once and use it until you're home.
             </p>
           </div>
