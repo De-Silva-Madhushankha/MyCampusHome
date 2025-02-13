47f960f41570fdeca2b1749b7e0a6e3469138f46
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-02-13 21:52:31 +0530

    deployed urls

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'mycampushome-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       }
