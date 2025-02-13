70513d31ae3604c59a5a3320110462784fea605b
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-02-13 21:56:48 +0530

    server url updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ import PayRent from './pages/PayRent';
 import NewChat from './pages/newChat';
 
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:4000/api';
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || 'mycampushome-production.up.railway.apphttps://mycampushome-production.up.railway.app/api/';
 
 
 export default function App() {
